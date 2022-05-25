618813ed54f30a9a2df2e2b5bc6fe8ad07031966
MdShakil14 <mdshakil1420414@gmail.com>
2022-05-25 18:07:59 +0600

    Implement add to cart page, search result page object model.

--- src/main/java/com/mystore/pageobjects/AddToCartPage.java
+++ src/main/java/com/mystore/pageobjects/AddToCartPage.java
@@ -1,4 +1,47 @@
 package com.mystore.pageobjects;
 
-public class AddToCartPage {
+import com.mystore.actiondriver.Action;
+import com.mystore.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class AddToCartPage extends BaseClass {
+    Action action = new Action();
+
+    public AddToCartPage() {
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(css = "#quantity_wanted")
+    private WebElement quantity;
+    @FindBy(css = "#group_1")
+    private WebElement size;
+    @FindBy(css = "button[name='Submit'] span")
+    private WebElement addToCartBtn;
+    @FindBy(xpath = "//i[@class='icon-ok']")
+    private WebElement addToCartMsg;
+    @FindBy(css = "a[title='Proceed to checkout'] span")
+    private WebElement proceedToCheckOut;
+
+    public void enterQuantity(String lquantity){
+        action.type(quantity,lquantity);
+    }
+
+    public void selectSize(String lsize){
+        action.selectByVisibleText(lsize,size);
+    }
+
+    public void clickAddToCart(){
+        action.click(driver,addToCartBtn);
+    }
+
+    public boolean validateAddToCart(){
+        return action.isDisplayed(driver,addToCartMsg);
+    }
+
+    public OrderPage validateProceedToCheckOut(){
+        action.JSClick(driver,proceedToCheckOut);
+        return new OrderPage();
+    }
 }
--- src/main/java/com/mystore/pageobjects/SearchResultPage.java
+++ src/main/java/com/mystore/pageobjects/SearchResultPage.java
@@ -1,4 +1,28 @@
 package com.mystore.pageobjects;
 
-public class SearchResultPage {
+import com.mystore.actiondriver.Action;
+import com.mystore.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchResultPage extends BaseClass {
+
+    Action action = new Action();
+
+    public SearchResultPage() {
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "//img[@title='Faded Short Sleeve T-shirts']")
+    WebElement productResult;
+
+    public boolean validateProductAvailability(){
+        return action.isDisplayed(driver,productResult);
+    }
+
+    public AddToCartPage clickOnProduct(){
+        action.click(driver,productResult);
+        return new AddToCartPage();
+    }
 }
